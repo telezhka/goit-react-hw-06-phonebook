ec0fad00a4b0a833d45465d37caf51271ca4b606
Roman Telik <rtelik58@gmail.com>
2023-09-10 13:42:56 +0300

    slice

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: { contacts: [], filter: '' },
+  reducers: {
+    addContact: (state, action) => {
+      state.contacts.push(action.payload);
+    },
+    deleteContact: (state, action) => {
+      state.contacts = state.contacts.filter(
+        contact => contact.id !== action.payload
+      );
+    },
+    updateFilter: (state, action) => {
+      state.filter = action.payload;
+    },
+  },
+});
+
+export const { addContact, deleteContact, updateFilter } =
+  contactsSlice.actions;
+export default contactsSlice.reducer;
